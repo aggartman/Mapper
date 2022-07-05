cbbecc767911920e33a2eb62e332be3861865ca3
Austin Gartman <aggartman@gmail.com>
2022-07-05 10:41:29 -0500

    Add map files and US_Cities csv data. Update py file with matplotlib, pandas, and numpy.

--- map_cities.py
+++ map_cities.py
@@ -5,12 +5,25 @@
  After connecting the provided cities to those in the US Database list, map the cities with their Rankings,
  City Name, on the correct Latitude and Longitude.
 """
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def read_city_list():
-    with open('uscities.csv', mode='r', encoding='utf-8') as file:
-        us_cities = file.read()
+    us_cities = pd.read_csv('uscities.csv')
+    us_cities.head()
+    bounding_box = ((us_cities.lng.min(), us_cities.lng.max(),
+                     us_cities.lat.min(), us_cities.lat.max()))
+    us_map = plt.imread('US_map.png')
+    fig, ax = plt.subplots(figsize=(8, 7))
+    ax.scatter(us_cities.lng, us_cities.lat, zorder=1, alpha=0.2, c='b', s=10)
+    ax.set_title('US Cities')
+    ax.set_xlim(bounding_box[0], bounding_box[1])
+    ax.set_ylim(bounding_box[2], bounding_box[3])
+    ax.imshow(us_map, zorder=0, extent=bounding_box, aspect='equal')
+    plt.show()
 
-        # add code here to match provided cities to us_cities
-        
-        file.close()
+
+if __name__ == "__main__":
+    read_city_list()
